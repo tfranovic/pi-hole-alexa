6404d78a1614a34d30537517ea8cfad73e33e03f
tfranovic <tin.franovic@gmail.com>
2019-11-07 15:58:39 -0500

    Basic code modifications to follow new model

--- lambda/py/pihole_alexa.py
+++ lambda/py/pihole_alexa.py
@@ -18,79 +18,78 @@ logger.setLevel(logging.INFO)
 
 
 @sb.request_handler(can_handle_func=is_request_type("LaunchRequest"))
-def launch_request_handler(handler_input):
+def launch_request_handler(handler_input: HandlerInput) -> Response:
     """Handler for Skill Launch."""
-    # type: (HandlerInput) -> Response
-    speech_text = "Welcome to the Alexa Skills Kit, you can say hello!"
+
+    speech_text = "Welcome to the Alexa Pi Hole skill, you can say 'show status'!"
 
     return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).set_should_end_session(
+        SimpleCard("PiHole Alexa", speech_text)).set_should_end_session(
         False).response
 
 
-@sb.request_handler(can_handle_func=is_intent_name("HelloWorldIntent"))
-def hello_world_intent_handler(handler_input):
-    """Handler for Hello World Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "Hello Python World from Decorators!"
+@sb.request_handler(can_handle_func=is_intent_name("StatusIntent"))
+def status_intent_handler(handler_input: HandlerInput) -> Response:
+    """Handler for Status Intent."""
+
+    speech_text = "I'm sorry, currently I can't display any status!"
 
     return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).set_should_end_session(
+        SimpleCard("PiHole Alexa", speech_text)).set_should_end_session(
         True).response
 
 
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.HelpIntent"))
-def help_intent_handler(handler_input):
+def help_intent_handler(handler_input: HandlerInput) -> Response:
     """Handler for Help Intent."""
-    # type: (HandlerInput) -> Response
-    speech_text = "You can say hello to me!"
+
+    speech_text = "You can say 'show status' to me!"
 
     return handler_input.response_builder.speak(speech_text).ask(
         speech_text).set_card(SimpleCard(
-            "Hello World", speech_text)).response
+            "PiHole Alexa", speech_text)).response
 
 
 @sb.request_handler(
     can_handle_func=lambda handler_input:
         is_intent_name("AMAZON.CancelIntent")(handler_input) or
         is_intent_name("AMAZON.StopIntent")(handler_input))
-def cancel_and_stop_intent_handler(handler_input):
+def cancel_and_stop_intent_handler(handler_input: HandlerInput) -> Response:
     """Single handler for Cancel and Stop Intent."""
-    # type: (HandlerInput) -> Response
+
     speech_text = "Goodbye!"
 
     return handler_input.response_builder.speak(speech_text).set_card(
-        SimpleCard("Hello World", speech_text)).response
+        SimpleCard("PiHole Alexa", speech_text)).response
 
 
 @sb.request_handler(can_handle_func=is_intent_name("AMAZON.FallbackIntent"))
-def fallback_handler(handler_input):
+def fallback_handler(handler_input: HandlerInput) -> Response:
     """AMAZON.FallbackIntent is only available in en-US locale.
     This handler will not be triggered except in that locale,
     so it is safe to deploy on any locale.
     """
-    # type: (HandlerInput) -> Response
     speech = (
-        "The Hello World skill can't help you with that.  "
-        "You can say hello!!")
-    reprompt = "You can say hello!!"
+        "The Pi Hole Alexa skill can't help you with that.  "
+        "You can say 'show status'!!")
+    reprompt = "You can say 'status' or 'show status'!!"
     handler_input.response_builder.speak(speech).ask(reprompt)
     return handler_input.response_builder.response
 
 
 @sb.request_handler(can_handle_func=is_request_type("SessionEndedRequest"))
-def session_ended_request_handler(handler_input):
+def session_ended_request_handler(handler_input: HandlerInput) -> Response:
     """Handler for Session End."""
-    # type: (HandlerInput) -> Response
+
     return handler_input.response_builder.response
 
 
 @sb.exception_handler(can_handle_func=lambda i, e: True)
-def all_exception_handler(handler_input, exception):
+def all_exception_handler(handler_input: HandlerInput, exception: Exception) -> Response:
     """Catch all exception handler, log exception and
     respond with custom message.
     """
-    # type: (HandlerInput, Exception) -> Response
+
     logger.error(exception, exc_info=True)
 
     speech = "Sorry, there was some problem. Please try again!!"
